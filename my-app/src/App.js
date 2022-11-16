ae3a4166336f6318661a7c7bb1f45427d2cef7e2
ssmi8 <smithsteve8@hotmail.com>
2022-11-16 16:56:25 +0000

    EventBinding

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,14 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
-import EventsClass from './components/EventsClass';
-import EventsFunctional from './components/EventsFunctional';
+import EventBinding from "./components/EventBinding";
 
 function App() {
   return (
     <div className="App">
-      <EventsFunctional/>
-      <EventsClass/>
-    </div>
+      <EventBinding/>
+      </div>
   );
 }
 
