37e8c28f1faf6228f95e924f11c390843c714959
ssmi8 <smithsteve8@hotmail.com>
2022-11-16 16:40:48 +0000

    Event handler

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,12 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
-import FunctionalGreetingWithProps from './components/FunctionalGreetingWithProps';
-import StatefulGreeting from './components/StatefulGreeting';
+import EventsClass from './components/EventsClass';
+import EventsFunctional from './components/EventsFunctional';
 
 function App() {
   return (
     <div className="App">
-      <StatefulGreeting/>
+      <EventsFunctional/>
+      <EventsClass/>
     </div>
   );
 }
