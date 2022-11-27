f7b135db40bbe8fddfa0a89a156bdb9c0c76da6b
ssmi8 <smithsteve8@hotmail.com>
2022-11-27 16:14:35 +0000

    Submitting form data

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,10 +1,11 @@
 import "./App.css";
-import LifeCyclesCDU from "./components/LifeCyclesCDU";
+import ControlledForm from "./components/ControlledForm";
+
 
 function App() {
   return <div className="App">
-      <LifeCyclesCDU />
-  </div>;
+      <ControlledForm />
+    </div>;
 }
 
 export default App;
