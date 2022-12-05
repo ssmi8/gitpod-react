2048b9ba5c04162e347696faf9276f151826a619
ssmi8 <smithsteve8@hotmail.com>
2022-12-05 19:00:53 +0000

    HTTP lessons

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,9 +1,10 @@
 import "./App.css";
-import UseEffectCounterContainer from "./components/UseEffectCounterContainer";
+import HTTPHooks from "./components/HTTPHooks";
+
 
 function App() {
   return <div className="App">
-      <UseEffectCounterContainer />
+      <HTTPHooks />
   </div>;
 }
 
